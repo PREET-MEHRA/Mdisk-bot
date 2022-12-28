252c15d61178f34cad483ae5d9c84bfef6d83d05
PREET-MEHRA <119394229+PREET-MEHRA@users.noreply.github.com>
2022-12-28 20:30:28 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo...."
-git clone 
-https://github.com/PREET-MEHRA/Mdisk-bot.git /Mdisk-bot
-cd /Mdisk-bot
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
